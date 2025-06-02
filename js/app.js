e184d71c2ccff2d900afc31e5fb3846cbd5847eb
Victor Gabriel Gutierrez Hernandez <160826547+VGabriel31@users.noreply.github.com>
2025-06-01 23:28:26 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -68,7 +68,7 @@ function ejecutarComandos() {
 async function enviarComando(command) {
     const data = {
         ip: ipPublica || "192.168.100.12",
-        name: "Gabriel Hernandez",
+        name: "Mariana Mendoza",
         status: command
     };
 
